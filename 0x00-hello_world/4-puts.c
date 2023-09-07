1d5ebfda496c12f92771d15da7310814bbd7725c
kelosykithinji <kelosykithinji@gmail.com>
2023-09-07 08:05:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main-My main point
+ * main - Entry Point
  *
- * Return:Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return(0);
 }
