2d7604803b2448784f13ca3d17565a1b29fd2971
kelosykithinji <kelosykithinji@gmail.com>
2023-09-12 06:05:43 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *void print_alphabet_x10 -function prints the alphabet in lowercase
+ *
+ *Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char n;
+
+	for (i = 0 ; i <= 10 ;  i++)
+	{
+		for (n = 'a' ; n <= 'z'; n++)
+			_putchar(n);
+		 _putchar('\n');
+	}
+}
