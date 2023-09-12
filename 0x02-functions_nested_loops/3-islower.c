875282c443abfeae0cd7966303b878a75664a217
kelosykithinji <kelosykithinji@gmail.com>
2023-09-12 12:48:01 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 /**
  *main - function checks for lowercase character
  *
- * Return 1 or 0
- * @c: parameter to be checked
+ *@c: parameter to be checked
+ *Return 1 or 0
  */
+
 int _islower(int c)
 {
-	if (c >= 'a' &&c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
