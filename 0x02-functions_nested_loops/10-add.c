860bdb20c3f278870571cd484aa25aac53195e10
kelosykithinji <kelosykithinji@gmail.com>
2023-09-13 07:06:25 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - adds two integers and returns the result
+ *@x: parameter 1
+ *@y: parameter 2
+ *Return: s
+ */
+int add(int x, int y)
+{
+	int s;
+
+	s = x + y;
+
+	return (s);
+}
